9e94961fbbd27b0bef06f35d9163babd48572b43
tusharx0809 <tusharbhatia0809@outlook.com>
2024-04-30 19:45:05 -0400

    added-functionality

--- data-structures/singally-linked-list-operations.py
+++ data-structures/singally-linked-list-operations.py
@@ -11,12 +11,30 @@ class Linked_list:
         self.head = None #initialize the head pointer
 
     def insert_at_head(self, data):
+        #create a new node with the data value using the object of Node
         new_node = Node(data)
-        if not self.head:
+        if not self.head: 
             self.head = new_node
         else:
             new_node.next = self.head
             self.head = new_node
+
+    def insert_at_position(self, data, position):
+        new_node = Node(data)
+        if position == 1:
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        temp = self.head
+
+        for _ in range(position - 2):
+            temp = temp.next
+
+        new_node.next = temp.next
+        temp.next = new_node
+
+
     def display_list(self):
         list_elements = []
         temp = self.head
@@ -26,6 +44,7 @@ class Linked_list:
         return ' -> '.join(list_elements)
     
 list1 = Linked_list()
-list1.insert_at_head(1)
 list1.insert_at_head(2)
+list1.insert_at_head(1)
+list1.insert_at_position(3,2)
 print(list1.display_list())
