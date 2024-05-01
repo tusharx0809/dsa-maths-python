acad91dbdc54654dd722d0df4f0e1420c41be022
tusharx0809 <tusharbhatia0809@outlook.com>
2024-04-30 22:49:29 -0400

    new-file-functions-defines

---
+++ mathematics/solve-equation.py
@@ -0,0 +1,6 @@
+"""Solving an equation following the rules of BODMAS using stack and polish notations"""
+
+def infix_to_postfix(equation):
+def infix_to_prefix(equation):
+def evaluate_prefix(equation):
+def evaluate_postfix(equation):
