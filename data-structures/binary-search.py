4deaccce7557a0b686c111eec445a52df6f53a5d
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-01 15:10:12 -0400

    binary-search-added

---
+++ data-structures/binary-search.py
@@ -0,0 +1,23 @@
+"""Below is the python program to perform binary search on arrays
+   Binary search only works on sorted arrays
+"""
+
+
+def binary_search(nums, target):
+    if len(nums) == 0:
+        print("Empty Array!")
+
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid #index location of the target element
+        elif nums[mid] < target:
+            left += 1
+        else:
+            right -= 1
+    return "Target not found!"
+nums = [4, 6, 9, 13, 87, 102, 203, 405]
+print(binary_search(nums, 103))
