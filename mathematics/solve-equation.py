dea5948eee70fd72027f35c7a9133d817858b7ee
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-01 21:16:46 -0400

    added-space-handling

--- mathematics/solve-equation.py
+++ mathematics/solve-equation.py
@@ -1,62 +1,59 @@
-"""Solving an equation following the rules of BODMAS using stack and postfix notations. Below code converts an infix notation to postfix and solve it"""
-
 def infix_to_postfix(equation):
-  operators = ['+','-','*','/','(',')','^']
-  prioritiy = { #set the priorities following BODMAS rules
-    '+': 1,
-    '-': 1,
-    '*': 2,
-    '/': 2,
-    '^': 3,
-  }
+    operators = set(['+', '-', '*', '/', '(', ')', '^'])
+    priority = {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+        '^': 3,
+    }
+    stack = []
+    postfix_notation = ''
+    
+    for char in equation:
+        if char.isdigit():  # Handle multi-digit numbers and decimals
+            postfix_notation += char
+        elif char == ' ':
+            continue
+        else:
+            postfix_notation += ' '
+            if char == '(':
+                stack.append(char)
+            elif char == ')':
+                while stack and stack[-1] != '(':
+                    postfix_notation += stack.pop() + ' '
+                stack.pop()
+            else:
+                while stack and stack[-1] != '(' and priority[char] <= priority.get(stack[-1], 0):
+                    postfix_notation += stack.pop() + ' '
+                stack.append(char)
+    
+    while stack:
+        postfix_notation += ' ' + stack.pop()
+    return postfix_notation
 
-  stack = []
-  postfix_notation = ''
+def evaluate_postfix(equation):
+    stack = []
+    tokens = equation.split()
+    for token in tokens:
+        if token in ['+','-','*','/','^']:
+            num_first = float(stack.pop())
+            num_second = float(stack.pop())
+            if token == '+':
+                stack.append(num_second + num_first)
+            elif token == '-':
+                stack.append(num_second - num_first)
+            elif token == '*':
+                stack.append(num_second * num_first)
+            elif token == '/':
+                stack.append(num_second / num_first)
+            elif token == '^':
+                stack.append(num_second ** num_first)
+        else:
+            stack.append(float(token))  # Ensure the numbers are pushed as floats
+    return stack.pop()
 
-  for char in equation:
-    if char not in operators:
-      postfix_notation += char
-    elif char == '(':
-      stack.append(char)
-    elif char == ')':
-      while stack and stack[-1] != '(':
-        postfix_notation += stack.pop()
-      stack.pop()
-    else:
-      while stack and stack[-1] != '(' and prioritiy[char] <= prioritiy[stack[-1]]:
-        postfix_notation += stack.pop()
-      stack.append(char)
-
-  while stack:
-    postfix_notation += stack.pop()
-  return postfix_notation
-
-def evaluate_postfix(equation): 
-  stack = []
-  equation = equation.replace(' ','') #to handle spaces in postfix notation input
-  tokens = list(equation)
-  for token in tokens:
-    if token in ['+','-','*','/','^']:
-      num_first = stack.pop()
-      num_second = stack.pop()
-      if token == '+':
-        stack.append(float(num_second) + float(num_first))
-      elif token == '-':
-        stack.append(float(num_second) - float(num_first))
-      elif token == '*':
-        stack.append(float(num_second) * float(num_first))
-      elif token == '/':
-        stack.append(float(num_second) / float(num_first))
-      else:
-        stack.append(float(num_second) ** float(num_first))
-    else:
-      stack.append(token)
-  return stack[0]
-
+# Testing with a complex expression
 postfix = infix_to_postfix('(3+4)*(5-2)/7^2')
-print(postfix)
-print("Solution below:")
-
-print(evaluate_postfix(postfix))
-
-
+print("Postfix:", postfix)
+print("Solution:", evaluate_postfix(postfix))
