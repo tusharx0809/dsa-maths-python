d6363c296bef7319e56f1f20060528b3e2d1faaa
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-01 21:58:39 -0400

    added-hash-map

---
+++ data-structures/hash-map.py
@@ -0,0 +1,19 @@
+"""Below is a basic implementation of hash-map in python - Hash-map stores key value pairs"""
+
+months = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+}
+
+for month_num, month_name in months.items():
+    print(month_num, month_name)
