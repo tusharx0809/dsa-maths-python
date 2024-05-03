522a6eca3e5056a625e7a0e8df2b9447814f0bbe
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-02 21:38:34 -0400

    file-added

---
+++ mathematics/solve-quadratic-equation.py
@@ -0,0 +1,7 @@
+"""Below program is used to calculate the roots of quadratic equation without using RegEx"""
+
+#Quadratic equation can be represented in the form of ax^2+bx+_c = 0 where
+#Formula to find the roots of a quadratic equation (-b +- ((math.sqrt(pow(b,2)-4*a*c))))  / (2 * a)
+
+def solve_quadratic(equation):
+    
