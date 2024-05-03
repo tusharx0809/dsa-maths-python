1cca70cf3547106c8861793d74205a23583f6562
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-02 21:53:37 -0400

    root-formula-added

--- mathematics/solve-quadratic-equation.py
+++ mathematics/solve-quadratic-equation.py
@@ -3,6 +3,7 @@
 #Quadratic equation can be represented in the form of ax^2+bx+_c = 0 where
 #Formula to find the roots of a quadratic equation (-b +- ((math.sqrt(pow(b,2)-4*a*c))))  / (2 * a)
 #equation used 
+import math
 def solve_quadratic(equation):
     equation = equation.replace(' ','')
     a = ''
@@ -31,6 +32,8 @@ def solve_quadratic(equation):
     b = int(b) if len(b) > 0 else 1
     c = int(c) if len(c) > 0 else 1
 
-    return a, b, c
+    first_root = (-b + ((math.sqrt(pow(b,2)-4*a*c))))  / (2 * a)
+    second_root = (-b - ((math.sqrt(pow(b,2)-4*a*c)))) / (2 * a)
+    return first_root, second_root
 
-print(solve_quadratic('2x^2-14x+24'))
+print(solve_quadratic('2x^2-14x+24')) # will print two roots of equation as (4.0, 3.0)
