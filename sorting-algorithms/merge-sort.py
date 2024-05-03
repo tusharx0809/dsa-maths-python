18cf4990629b1799b82fb210645519864bb85f87
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-03 15:11:12 -0400

    merge-sort-added

--- sorting-algorithms/merge-sort.py
+++ sorting-algorithms/merge-sort.py
@@ -7,7 +7,28 @@ def merge_sort(nums):
     left = nums[:mid]
     right = nums[mid:]
 
-    return merge(merge_sort(left, right))
+    return merge(merge_sort(left), merge_sort(right))
 
+def merge(left, right):
+    merged = []
+    left_idx = 0
+    right_idx = 0
 
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] <= right[right_idx]:
+            merged.append(left[left_idx])
+            left_idx += 1
+        else:
+            merged.append(right[right_idx])
+            right_idx += 1
 
+    merged.extend(left[left_idx:])
+    merged.extend(right[right_idx:])
+
+    return merged
+
+array = [5,4,3,2,1]
+sorted_array = merge_sort(array)
+
+print(sorted_array)
+    
