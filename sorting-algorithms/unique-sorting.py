65bfceb21ba6f25d696a6494b41c82220a821a65
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-03 16:16:08 -0400

    descending-added

--- sorting-algorithms/unique-sorting.py
+++ sorting-algorithms/unique-sorting.py
@@ -10,7 +10,7 @@ Repeat this until nums is empty
 final array will be the sorted array
 
 """
-def unique_sort_ascending(nums):
+def unique_sort(nums):
   final = []
   while len(nums) != 0:
       if len(nums) == 1:
@@ -19,14 +19,14 @@ def unique_sort_ascending(nums):
         nums.remove(nums[0])
       else:
         if len(final) == 0:
-          final.append(min(nums))
-          final.append(max(nums))
+          final.append(min(nums)) # change this to final.append(max(nums)) to sort it descending order
+          final.append(max(nums)) # change this to final.append(min(nums)) to sort it descending order
         else:
           mid = len(final) // 2
-          final.insert(mid,min(nums))
-          final.insert(mid+1,max(nums))
+          final.insert(mid,min(nums)) # change this to final.insert(mid,max(nums)) tp
+          final.insert(mid+1,max(nums)) # change this to final.insert(mid+1,min(nums)) to sort it descending order
         nums.remove(min(nums))
         nums.remove(max(nums))
   return final 
 nums = [100,98,12,45,67,43]
-print("Ascending:",unique_sort_ascending(nums))
+print(unique_sort(nums))
