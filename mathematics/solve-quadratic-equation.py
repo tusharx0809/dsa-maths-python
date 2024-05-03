9d11a72d8049b42f00e2986167db957c2312fd6d
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-02 22:13:27 -0400

    minor-changes

--- mathematics/solve-quadratic-equation.py
+++ mathematics/solve-quadratic-equation.py
@@ -1,6 +1,6 @@
 """Below program is used to calculate the roots of quadratic equation without using RegEx"""
 
-#Quadratic equation can be represented in the form of ax^2+bx+_c = 0 where
+#Quadratic equation can be represented in the form of ax**2+b*x+_c = 0 where
 #Formula to find the roots of a quadratic equation (-b +- ((math.sqrt(pow(b,2)-4*a*c))))  / (2 * a)
 #equation used 
 import math
@@ -13,14 +13,14 @@ def solve_quadratic(equation):
     while equation[index].isdigit() or equation[index] == '-':
         a += equation[index]
         index += 1
-    index += 3 # skip x^2
+    index += 4 # skip x**2
     if equation[index] == '-' or equation[index] == '+':
         b += equation[index]
         index += 1
     while equation[index].isdigit():
         b += equation[index]
         index += 1
-    index += 1 # skip x
+    index += 2 # skip *x
     if equation[index] == '-' or equation[index] == '+':
         c += equation[index]
         index += 1
@@ -39,6 +39,6 @@ def solve_quadratic(equation):
         second_root = (-b - ((math.sqrt(pow(b,2)-4*a*c)))) / (2 * a)
         return first_root, second_root
 
-print(solve_quadratic('2x^2-14x+24')) # will print two roots of equation as (4.0, 3.0)
-print(solve_quadratic('x^2-2x+2'))# this will print No real roots exists for this equation!
-print(solve_quadratic('2x^2-9x+5'))# will print two roots of equation as (3.850781059358212, 0.6492189406417879)
+print(solve_quadratic('2x**2-14*x+24')) # will print two roots of equation as (4.0, 3.0)
+print(solve_quadratic('x**2-2*x+2'))# this will print No real roots exists for this equation!
+print(solve_quadratic('2x**2-9*x+5'))# will print two roots of equation as (3.850781059358212, 0.6492189406417879)
