88522885249fadf7b2bb679c90dc0891532c9c2e
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-04 13:54:10 -0400

    reverse-added

--- mathematics/some-basic-functions.py
+++ mathematics/some-basic-functions.py
@@ -12,7 +12,6 @@ num = 123
 print("The sum of digits", num ,"is: ", sum_of_digits(num)) # The sum of digits 123 is:  6
 
 
-
 #Sum of digits using python inbuilt functionality
 def sum_Of_digits(num):
     sum_Of_digits = sum(int(i) for i in str(num))
@@ -20,4 +19,16 @@ def sum_Of_digits(num):
 num2 = 456
 print("The sum of digits", num2 ,"is: ", sum_of_digits(num2)) # The sum of digits 456 is:  15
 
+
+# Finding the reverse of a number 
+def reverse(num):
+    reversed_num = 0
+    while num > 0:
+        rem = num % 10
+        reversed_num = (reversed_num * 10) + rem
+        num = num // 10
+    return reversed_num
+
+num3 = 123
+print("The reverse of", num3, "is:", reverse(num3)) # The reverse of 123 is: 321
     
